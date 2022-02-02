74a4476508ce05bd622a0ee7c8443643a0d30360
mavisyupyup1 <mavisyupyup1@github.com>
2022-02-02 10:40:15 -0500

    comment model

--- server.js
+++ server.js
@@ -15,6 +15,6 @@ mongoose.connect(process.env.MONGODB_URI ||'mongodb://localhost:27017/pizza-hunt
 
 
 
-// Use this to log mongo queries being excuted!
+// Use this to log mongo queries being executed!
 mongoose.set('debug',true);
 app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
